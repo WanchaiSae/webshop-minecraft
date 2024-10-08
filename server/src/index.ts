c8f3bb7b44103444955c200def6bdb5a7432c221
WanchaiSae <wanchai.sae@outlook.com>
2024-10-08 17:05:51 +0700

    mock up

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,14 @@ config();
 import configOptions from './utils/serverConfigs';
 
 import express from 'express';
-import connection from './config/mysqlConfig';
+
+import authRoutes from './routes/auth';
 
 const app = express();
 
 const PORT = configOptions.PORT;
+
+// Routes
+app.use('/user', authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
