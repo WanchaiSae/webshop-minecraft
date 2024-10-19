6db0f00d7c270acb68dcff477debbb0a594b5e07
WanchaiSae <wanchai.sae@outlook.com>
2024-10-19 16:26:37 +0700

    Updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import NavBar from './pages/NavBar'
 import ListItems from './components/items/ListItems'
+import NavBar from './pages/NavBar'
 function App() {
 
   const navigate = useNavigate()
